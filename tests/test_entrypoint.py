c4f0098c2d0dcd22237b35ceadecb8ae443acc80
Antonio <antonio.one@pm.me>
2021-03-23 19:49:24 +0000

    adding test_retrieve_no_content_from_api_v1

--- tests/test_entrypoint.py
+++ tests/test_entrypoint.py
@@ -76,3 +76,38 @@ def test_retrieve_schema_from_api_v1(test_client, schemas_path):
         actual = response.json()
         expected = json.loads(sp.read())
         assert actual == expected
+
+
+@pytest.mark.v0
+@pytest.mark.parametrize(
+    "schema_name, schema_version, expected_status, expected_text",
+    [
+        ("non_existing_schema", 1, 200, 404),
+        ("schema_one", 3, 200, 404),
+        (
+            "schema_one",
+            "unexpected_string",
+            422,
+            {
+                "detail": [
+                    {
+                        "loc": ["path", "version"],
+                        "msg": "value is not a valid integer",
+                        "type": "type_error.integer",
+                    }
+                ]
+            },
+        ),
+        (101, 1, 200, 404),
+        ("schema_two", -1, 200, 404),
+        ("schema_two", 0, 200, 404),
+    ],
+)
+def test_retrieve_no_content_from_api_v1(
+    test_client, schema_name, schema_version, expected_status, expected_text
+):
+    url = f"/v1/datcat/schemas/get/{schema_name}/version/{schema_version}/refresh/true"
+    response = test_client.get(url)
+
+    assert response.status_code == expected_status
+    assert response.json() == expected_text
